3bee3b572dc2d7e92baf518e3bc50ce5f3980064
Catherine Lavin <cmlavin7@gmail.com>
2017-05-27 23:41:51 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,8 @@
 def alphabetize(arr)
-  # code here
+  esp_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  arr.sort_by do |phrase|
+    phrase.chars.map do |char|
+      esp_alphabet.index(char)
+    end
+  end
 end
